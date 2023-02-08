bbf8d17acad4836241a358d41a3f73a4774214cf
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2023-02-08 16:25:11 -0300

    treat empty jobs
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- src/components/AvailableJobs/index.jsx
+++ src/components/AvailableJobs/index.jsx
@@ -6,10 +6,21 @@ function AvailableJobs({ jobs }) {
     <section id="elo7-jobs" className={styles.availableJobsContainer}>
       <h2>Vagas em aberto</h2>
       <article className={styles.developmentJobs}>
-        <h3>Desenvolvimento</h3>
-        {jobs.map(({ role, link, location }) => (
-          <Job key={role} role={role} link={link} location={location} />
-        ))}
+        {jobs.length === 0 ? (
+          <p>Não há vagas disponíveis.</p>
+        ) : (
+          <>
+            <h3>Desenvolvimento</h3>
+            {jobs.map(({ role, link, location }, index) => (
+              <Job
+                key={`${role}-${index}`}
+                role={role}
+                link={link}
+                location={location}
+              />
+            ))}
+          </>
+        )}
       </article>
     </section>
   );
